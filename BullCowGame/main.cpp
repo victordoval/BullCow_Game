3940e0b61156da5fc241c949b1864928c7acc2f4
Victor do Val <victordoval1@hotmail.com>
2016-08-04 18:04:53 -0500

    Add project files.

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+int main()
+{
+	PrintIntro();
+
+	// Loop for the number of turns asking for guesses
+	constexpr int NUMBER_OF_TURNS = 5;
+	for (int count = 1; count <= NUMBER_OF_TURNS; count++)
+	{
+		string Input = GetGuessAndPrintBack();
+		cout << endl;
+	}
+	return 0;
+}
+
+// Introduce the game
+void PrintIntro()
+{
+	constexpr int WORD_LENGTH = 9;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of?\n";
+	cout << endl;
+}
+
+string GetGuessAndPrintBack()
+{
+	// Get a guess from the player
+	string Guess = "";
+	cout << "Enter your guess: ";
+	getline(cin, Guess);
+
+	// Repeat the guess back to the player
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
