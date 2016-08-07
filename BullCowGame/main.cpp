6f5531523e427413b380e899c44076944f48d985
Victor do Val <victordoval1@hotmail.com>
2016-08-07 01:38:44 -0500

    Added ClearScreen function to clear the screen for when the game is reset

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -15,6 +15,7 @@ using int32 = int;
 
 // function prototypes as outside a class
 void ResizeConsole();
+void ClearScreen();
 void PrintIntro();
 void PlayGame();
 FText GetValidGuess();
@@ -28,6 +29,7 @@ int32 main()
 	ResizeConsole(); // resizes console window for the ASCII art to fit
 
 	do {
+		ClearScreen();
 		PrintIntro();
 		PlayGame();
 	} 
@@ -45,6 +47,43 @@ void ResizeConsole()
 	//MoveWindow(window_handle, x, y, width, height, redraw_window);
 	MoveWindow(console, r.left, r.top, 600, 800, TRUE);
 	return;
+}
+
+void ClearScreen()
+{
+	HANDLE                     hStdOut;
+	CONSOLE_SCREEN_BUFFER_INFO csbi;
+	DWORD                      count;
+	DWORD                      cellCount;
+	COORD                      homeCoords = { 0, 0 };
+
+	hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdOut == INVALID_HANDLE_VALUE) return;
+
+	/* Get the number of cells in the current buffer */
+	if (!GetConsoleScreenBufferInfo(hStdOut, &csbi)) return;
+	cellCount = csbi.dwSize.X *csbi.dwSize.Y;
+
+	/* Fill the entire buffer with spaces */
+	if (!FillConsoleOutputCharacter(
+		hStdOut,
+		(TCHAR) ' ',
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Fill the entire buffer with the current colors and attributes */
+	if (!FillConsoleOutputAttribute(
+		hStdOut,
+		csbi.wAttributes,
+		cellCount,
+		homeCoords,
+		&count
+	)) return;
+
+	/* Move the cursor home */
+	SetConsoleCursorPosition(hStdOut, homeCoords);
 }
 
 void PrintIntro()
